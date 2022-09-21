2ba15015c005dd924ff1cf29c96eb32cfb1ed0e5
Arya Buddha <40647186+AryaBuddha@users.noreply.github.com>
2022-09-20 20:31:58 -0400

    Accidental local API

--- src/API/api.js
+++ src/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const url = "https://schedule-client-api.herokuapp.com"
-const url = "http://localhost:5000";
+const url = "https://schedule-client-api.herokuapp.com";
+//const url = "http://localhost:5000";
 const newsUrl = "https://hsenews.com/wp-json/wp/v2/posts/?categories=493";
 
 export const getSchedule = () => {
